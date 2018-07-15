0f0b8601466ad77560ddb80686146e2fcf29f36e
younes <youneskasri@gmail.com>
2018-07-16 00:58:51 +0100

    Factorize routes for RestController

--- src/main/java/ma/map/tm/web/ProjectController.java
+++ src/main/java/ma/map/tm/web/ProjectController.java
@@ -10,39 +10,41 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ma.map.tm.business.IProjectService;
 import ma.map.tm.entities.ProjectDTO;
 
 @RestController
+@RequestMapping("/projects")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ProjectController {
 
 	@Autowired
 	private IProjectService projectService;
 	
-	@GetMapping("/projects")
+	@GetMapping
 	public List<ProjectDTO> index(){
 		return projectService.retrieveAllProjects();
 	}
 	
-	@PostMapping("/projects")
+	@PostMapping
 	public ProjectDTO create(@RequestBody ProjectForm data) {
 		return projectService.createProject(data);
 	}
 	
-	@GetMapping("/projects/{id}")
+	@GetMapping("/{id}")
 	public ProjectDTO show(@PathVariable Long id) {
 		return projectService.retrieveProjectById(id);
 	}
 
-	@PatchMapping("/projects/{id}")
+	@PatchMapping("/{id}")
 	public ProjectDTO update(@PathVariable Long id, @RequestBody ProjectForm data) {
 		return projectService.updateProject(id, data);
 	}
 	
-	@DeleteMapping("/projects/{id}")
+	@DeleteMapping("/{id}")
 	public Boolean destroy(@PathVariable Long id) {
 		return projectService.removeProjectById(id);
 	}
